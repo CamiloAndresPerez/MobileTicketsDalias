e3a6db21d992260318a5a4324db03ac65b5e1bfa
CamiloAndresPerez <camilo.perez02@uptc.edu.co>
2023-05-01 12:26:14 -0500

    variables de entorno

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 import { config } from "dotenv";
 config();
 
-export const PORT = process.env.PORT || 3000;
-export const MONGODB_URI = process.env.MONGODB_URI || "mongodb+srv://camiloperez02:camilo1234@daliaspasteleriadatabas.lk4wiff.mongodb.net/daliasdb";
+export const PORT = process.env.PORT;
+export const MONGODB_URI ="mongodb+srv://camiloperez02:camilo1234@daliaspasteleriadatabas.lk4wiff.mongodb.net/daliasdb";
